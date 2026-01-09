85a6667dc4eca42338a6562199bc3e00c68f05d6
Kiril <kiril@Mac-jenya.local>
2026-01-09 17:43:40 +0200

    googleads-sitemaps-robots

--- src/components/ListViews/ListViews.jsx
+++ src/components/ListViews/ListViews.jsx
@@ -15,12 +15,12 @@ function ListViews(){
             <div className="container">
                 <div className="row justify-content-center align-items-center">
                 {projects.map(project => (
-                    <div className="card"  style={{ width: '320px' }}>
+                    <div key={project.id}  className="card"  style={{ width: '320px' }}>
                         <Link to={`/projects/${project.slug}`}>
                             <img src={project.image} alt={project.name}  style={{ height: '400px', objectFit: 'cover', borderRadius:'30px' }}/>
                         </Link>  
-                        <div class="card-body">
-                            <div class="card-text">
+                        <div className="card-body">
+                            <div className="card-text">
                                 <h5>{project.name}</h5>
                             </div>
                         </div>
@@ -29,21 +29,6 @@ function ListViews(){
                 </div>
             </div>
         </section>
-
-        // <div>
-        //     <h2>Projects</h2>
-        //     <ul>
-        //         {projects.map(project => (
-        //         <li key={project.id}>
-        //             <h3>{project.name}</h3>
-        //             <p>{project.tehnology_project}</p>
-        //             <a href={project.link} target="_blank" rel="noreferrer">Link</a>
-        //             <img alt={project.name} src={project.image} style={{width:'100px',height:'100px'}}/>
-        //             <Link to={`/projects/${project.slug}`}>Смотреть подробнее</Link>
-        //         </li>
-        //         ))}
-        //     </ul>
-        // </div>
     );
 }
 
